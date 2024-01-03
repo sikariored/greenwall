482979ce9f3028635d375b24a644f031d98d3eb1
Alexander <admin@itpro.spb.ru>
2024-01-03 15:14:51 +0300

    STABLE: Выдача доступов к записям отделов у отдельных акк-ов через админку

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_03_070439) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_03_065937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,7 +52,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_03_070439) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "department_access", default: [], array: true
   end
 
   create_table "secure_records", force: :cascade do |t|
